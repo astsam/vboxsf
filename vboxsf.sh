d44c1b8949a6404102983274db25d0e5c6ee56a6
astsam <serg@no-problem.cc>
2018-03-23 13:56:11 +0300

    Mount shared folder into /mnt/media_rw directory

--- vboxsf.sh
+++ vboxsf.sh
@@ -1,8 +1,14 @@
-#!/system/bn/sh
-GID=3003
-UID=3003
+#!/system/bin/sh
+# 1015 - sdcard_rw, 1023 - media_rw, 3003 - inet
+# 1023: /mnt/media_rw
+#GID=3003
+#UID=3003
+#dir=/mnt/vboxsf
+GID=1023
+UID=1023
+dir=/mnt/media_rw/vboxsf
 modprobe vboxsf
-mkdir /mnt/vboxsf
-chown $UID.$GID /mnt/vboxsf
-/system/bin/mount.vboxsf -o uid=$UID,gid=$GID,dmode=0777 vboxsf /mnt/vboxsf
+mkdir $dir
+chown $UID.$GID $dir
+/system/bin/mount.vboxsf -o uid=$UID,gid=$GID,dmode=0777 vboxsf $dir
 
